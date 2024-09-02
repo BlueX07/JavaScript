a17fea0b09aebcc213f35addabbf0dc6fb075576
Lucas07_ <154766444+BlueX07@users.noreply.github.com>
2024-09-02 15:52:10 -0300

    Funções

---
+++ funcoes.js
@@ -0,0 +1,15 @@
+bomDia = function(){
+    console.log("Bom Dia!");
+}
+bomDia();
+
+function boaTarde(){
+    console.log("Boa Tarde!");
+}
+boaTarde();
+
+function soma(){
+    let resultado = 1 + 2;
+    console.log(resultado);
+}
+soma();
